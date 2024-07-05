d8ae523357e9b2db17defd86405110e4811e3b7d
Hender Orlando Puello <hender.puello@gmail.com>
2024-07-05 11:35:40 -0500

    Added list to swagger

--- apps/b2crypto/src/main-gateway.ts
+++ apps/b2crypto/src/main-gateway.ts
@@ -67,6 +67,7 @@ function addSwaggerStakeyCard(app: INestApplication) {
     .addTag('Stakey Profile')
     .addTag('Stakey Deposit')
     .addTag('Stakey Card')
+    .addTag('Stakey List')
     /* .addApiKey(
       {
         type: 'apiKey',
