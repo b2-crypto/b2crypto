8b407eff66af16dd33d6951d432cd1b4d53af074
Gabriel Munera <gabriel.munera@b2crypto.com>
2024-07-18 17:29:23 -0500

    Configs for integration service

--- apps/integration-service/src/main.ts
+++ apps/integration-service/src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { IntegrationServiceModule } from './integration-service.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(IntegrationServiceModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(IntegrationServiceModule, {
+    // logger: false,
+  });
+  app.enableCors();
+  await app.listen(process.env.PORT ?? 3000);
+  if (typeof process.send === 'function') {
+    process.send('ready');
+  }
 }
 bootstrap();
