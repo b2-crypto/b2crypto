278505fc17d8f6a1c3684405c0153e2d824dd134
Gabriel Munera <gabriel.munera@b2crypto.com>
2024-07-26 08:44:45 -0500

    Adds shipping hooks

---
+++ libs/integration/src/dto/pomelo.shipping.body.dto.ts
@@ -0,0 +1,56 @@
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsString, ValidateNested } from 'class-validator';
+
+class ShippingMeta {
+  @IsNotEmpty()
+  @IsString()
+  resource_url: string;
+}
+
+export class ShippingNotifications {
+  @IsNotEmpty()
+  @IsString()
+  event_id: string;
+  @IsNotEmpty()
+  @IsString()
+  shipment_id: string;
+  @IsNotEmpty()
+  @IsString()
+  status: string;
+  @IsNotEmpty()
+  @IsString()
+  status_detail: string;
+  @IsNotEmpty()
+  @IsString()
+  request_status: string;
+  @IsNotEmpty()
+  @IsString()
+  updated_at: string;
+  @ValidateNested()
+  @Type(() => ShippingMeta)
+  meta: ShippingMeta;
+  @IsNotEmpty()
+  @IsString()
+  idempotency_key: string;
+}
+
+export class CardEvents {
+  @IsNotEmpty()
+  @IsString()
+  event_id: string;
+  @IsNotEmpty()
+  @IsString()
+  id: string;
+  @IsNotEmpty()
+  @IsString()
+  updated_at: string;
+  @IsNotEmpty()
+  @IsString()
+  user_id: string;
+  @IsNotEmpty()
+  @IsString()
+  event: string;
+  @IsNotEmpty()
+  @IsString()
+  idempotency_key: string;
+}
