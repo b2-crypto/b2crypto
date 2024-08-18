2c0633bdbf71ab887cfd3a2fb05d38ddd490bd89
Hender Orlando Puello <hender.puello@gmail.com>
2024-08-18 14:18:11 -0500

    Added categoryParent and groups to category

--- libs/category/src/entities/category.entity.ts
+++ libs/category/src/entities/category.entity.ts
@@ -1,13 +1,8 @@
-import { ActivityEntity } from '@activity/activity/entities/activity.entity';
 import { CategoryInterface } from '@category/category/entities/category.interface';
 import ResourcesEnum from '@common/common/enums/ResourceEnum';
 import TagEnum from '@common/common/enums/TagEnum';
-import { CrmEntity } from '@crm/crm/entities/crm.entity';
-import { FileEntity } from '@file/file/entities/file.entity';
 import { GroupEntity } from '@group/group/entities/group.entity';
 import { ApiProperty } from '@nestjs/swagger';
-import { PspEntity } from '@psp/psp/entities/psp.entity';
-import { RoleEntity } from '@role/role/entities/role.entity';
 import { ObjectId } from 'mongoose';
 
 export class CategoryEntity implements CategoryInterface {
@@ -26,13 +21,9 @@ export class CategoryEntity implements CategoryInterface {
   description: string;
   searchText: string;
   type: TagEnum;
-  resources: ResourcesEnum[];
-  roles: RoleEntity[];
-  files: FileEntity[];
+  categoryParent: CategoryEntity;
   groups: GroupEntity[];
-  crms: CrmEntity[];
-  psps: PspEntity[];
-  activities: ActivityEntity[];
+  resources: ResourcesEnum[];
   createdAt: Date;
   updatedAt: Date;
   valueNumber: number;
