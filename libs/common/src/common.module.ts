b5f7473dd918aa543b47ab09b5b1f61e8650ebb8
Hender Orlando Puello <hender.puello@gmail.com>
2024-08-21 02:09:47 -0500

    Remove Cache config in CommonModule

--- libs/common/src/common.module.ts
+++ libs/common/src/common.module.ts
@@ -1,4 +1,4 @@
-import { CacheModule, Logger, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CommonService } from './common.service';
 import { databaseProviders } from './database-providers/database-providers.service';
 import { IntegrationModule } from '@integration/integration';
@@ -7,39 +7,8 @@ import { PomeloSignatureUtils } from './utils/pomelo.integration.process.signatu
 import { PomeloHttpUtils } from './utils/pomelo.integration.process.http.utils';
 import { PomeloCache } from '@integration/integration/util/pomelo.integration.process.cache';
 import { PomeloProcessConstants } from './utils/pomelo.integration.process.constants';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { IntegrationServiceModule } from 'apps/integration-service/src/integration-service.module';
-import { redisStore } from 'cache-manager-redis-store';
-import { RedisClientOptions } from 'redis';
 
 @Module({
-  imports: [
-    CacheModule.registerAsync({
-      imports: [ConfigModule, IntegrationServiceModule],
-      useFactory: async (configService: ConfigService) => {
-        const config = {
-          store: redisStore,
-          username: configService.get('REDIS_USERNAME') ?? '',
-          password: configService.get('REDIS_PASSWORD') ?? '',
-          host: configService.get('REDIS_HOST') ?? 'localhost',
-          port: configService.get('REDIS_PORT') ?? 6379,
-          ttl: parseInt(configService.get('CACHE_TTL') ?? '20') * 1000,
-          max: parseInt(configService.get('CACHE_MAX_ITEMS') ?? '10'),
-          isGlobal: true,
-        } as RedisClientOptions;
-        Logger.log(config, 'Redis Config');
-        if (!config.password || !config.username) {
-          return {
-            ttl: parseInt(configService.get('CACHE_TTL') ?? '20') * 1000,
-            max: parseInt(configService.get('CACHE_MAX_ITEMS') ?? '10'),
-            isGlobal: true,
-          };
-        }
-        return config;
-      },
-      inject: [ConfigService],
-    }),
-  ],
   providers: [
     PomeloCache,
     CommonService,
