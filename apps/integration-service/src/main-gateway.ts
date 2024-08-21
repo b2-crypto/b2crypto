a629c1f7b6335d76eb5d0f3585d214de88247c82
Hender Orlando Puello <hender.puello@gmail.com>
2024-08-21 00:16:42 -0500

    Update file name

--- apps/integration-service/src/main-gateway.ts
+++ apps/integration-service/src/main-gateway.ts
@@ -2,10 +2,4 @@ import { EnvironmentEnum } from '@common/common/enums/environment.enum';
 import { bootstrapGateway } from '@common/common/models/bootstrap.gateway';
 import { IntegrationServiceModule } from './integration-service.module';
 
-bootstrapGateway(
-  IntegrationServiceModule,
-  process.env.environment === EnvironmentEnum.prod
-    ? EnvironmentEnum.prod
-    : EnvironmentEnum.dev,
-  3001,
-);
+bootstrapGateway(IntegrationServiceModule, EnvironmentEnum.prod, 3001);
