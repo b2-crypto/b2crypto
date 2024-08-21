4fb678ab923e8b4a5021973a7f03efe8df328367
Hender Orlando Puello <hender.puello@gmail.com>
2024-08-21 00:04:55 -0500

    Integration service pm2 deploy

--- apps/integration-service/src/main.ts
+++ apps/integration-service/src/main.ts
@@ -2,4 +2,10 @@ import { EnvironmentEnum } from '@common/common/enums/environment.enum';
 import { bootstrapGateway } from '@common/common/models/bootstrap.gateway';
 import { IntegrationServiceModule } from './integration-service.module';
 
-bootstrapGateway(IntegrationServiceModule, EnvironmentEnum.dev, 3001);
+bootstrapGateway(
+  IntegrationServiceModule,
+  process.env.environment === EnvironmentEnum.prod
+    ? EnvironmentEnum.prod
+    : EnvironmentEnum.dev,
+  3001,
+);
