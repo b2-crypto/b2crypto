ce9d020d10ffdd43287bee3d153da2f5b9d40967
Hender Orlando Puello <hender.puello@gmail.com>
2024-08-30 11:04:30 -0500

    Added properties swagger, fixed transfer/me tag and fixed recharge wallet from wallet

--- libs/account/src/dto/card-deposit.create.dto.ts
+++ libs/account/src/dto/card-deposit.create.dto.ts
@@ -1,19 +1,36 @@
 import { CreateAnyDto } from '@common/common/models/create-any.dto';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsMongoId, IsNumber, IsOptional, IsString } from 'class-validator';
 import { ObjectId } from 'mongoose';
 
 export class CardDepositCreateDto extends CreateAnyDto {
+  @ApiProperty({
+    required: true,
+    description: 'Card to arrive',
+  })
   @IsMongoId()
   to: ObjectId;
 
+  @ApiProperty({
+    required: false,
+    description: 'Wallet to comes from',
+  })
   @IsMongoId()
   @IsOptional()
   from?: ObjectId;
 
+  @ApiProperty({
+    required: false,
+    description: 'Code to execute the transaction',
+  })
   @IsString()
   @IsOptional()
   pin: string;
 
+  @ApiProperty({
+    required: true,
+    description: 'Amount to withdraw from the wallet to the card',
+  })
   @IsNumber({
     allowNaN: false,
     allowInfinity: false,
@@ -22,5 +39,5 @@ export class CardDepositCreateDto extends CreateAnyDto {
 
   @IsString()
   @IsOptional()
-  movement?: string;
+  movement?: string; // Movement: Credit, Debit
 }
