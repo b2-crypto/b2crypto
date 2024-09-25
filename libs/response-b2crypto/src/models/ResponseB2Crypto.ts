0474f403dfffbb5307bd1d12592f460f5fc737e4
apoveda25 <alfpovsistemas@gmail.com>
2024-09-25 16:13:52 -0500

    ci: 👷 change response handler

--- libs/response-b2crypto/src/models/ResponseB2Crypto.ts
+++ libs/response-b2crypto/src/models/ResponseB2Crypto.ts
@@ -16,28 +16,6 @@ class ResponseB2Crypto {
     private description?: string,
     private page?: any,
   ) {
-    // if (data.response) {
-    //   data = {
-    //     ...data.response,
-    //     message: data.message ?? data.response.message,
-    //     status: data.status ?? data.response.status,
-    //   };
-    // }
-    // if (data.response) {
-    //   data = {
-    //     ...data.response,
-    //     message: data.message ?? data.response.message,
-    //     statusCode: data.statusCode ?? data.response.statusCode,
-    //   };
-    // }
-    // if (data.response) {
-    //   data = {
-    //     ...data.response,
-    //     message: data.message ?? data.response.message,
-    //     statusCode: data.statusCode ?? data.response.statusCode,
-    //   };
-    // }
-
     this.code = code || this.getCode(data);
     this.action = action;
     this.message = message || data?.message || data?.response?.message;
@@ -47,15 +25,6 @@ class ResponseB2Crypto {
       Array.isArray(data) || !!data?.id || data?.access_token
         ? data
         : data?.data;
-
-    // if (this.env === EnvironmentEnum.dev) {
-    //   this.data = data;
-    // } else {
-    //   if (isArray(data) || !!data?.id || data.access_token) {
-    //     // Is list or object or update
-    //     this.data = data;
-    //   }
-    // }
 
     if (data.nextPage) {
       this.page = {
@@ -113,29 +82,6 @@ class ResponseB2Crypto {
     }
 
     return statusCode;
-
-    // let code: number = (data?.code ||
-    //   data?.statusCode ||
-    //   data?.status) as number;
-    // if (!code || code < 200 || code > 504) {
-    //   // TODO[hender] Validate the other error to return HTTP statusCode
-    //   code = 500;
-    //   if (
-    //     isArray(data) ||
-    //     data?.id?.hasOwnProperty('_bsontype') ||
-    //     data?.data?.id?.hasOwnProperty('_bsontype')
-    //   ) {
-    //     // Is list or object
-    //     code = 200;
-    //   } else if (!!data?.stack) {
-    //     code = 501;
-    //   } else if (data?.hasOwnProperty('affected')) {
-    //     // Is update
-    //     code = data.affected ? 200 : 400;
-    //   }
-    // }
-    // delete this.data?.statusCode;
-    // return code;
   }
 
   getResponse(message?: string, description?: string) {
@@ -155,34 +101,7 @@ class ResponseB2Crypto {
       data: this.data?.list ?? this.data,
       page: this.page,
     };
-
-    // if (!this.code) {
-    //   // TODO[hender] Check if is no code mean is an error
-    //   this.code = 500;
-    // }
-    // message = message ?? this.message;
-    // if (!message && !this.message) {
-    //   message = MessageResponseB2Crypto.getMessageCode(this.code, this.action);
-    // } else if (!message) {
-    //   message = MessageResponseB2Crypto.getMessageCode(this.code, this.action);
-    //   description = this.getDescription(description);
-    // }
-    // return {
-    //   statusCode: this.code,
-    //   message: message,
-    //   description: description || this.description,
-    //   data: this.data?.list ?? this.data,
-    //   page: this.page,
-    // };
   }
-
-  // private getDescription(description): string {
-  //   const desc = this.data?.response?.message || this.message || description;
-  //   if (isArray(desc)) {
-  //     return desc.join(', ');
-  //   }
-  //   return desc;
-  // }
 
   getErrorResponse(): BadRequestException {
     const rta = this.getResponse();
