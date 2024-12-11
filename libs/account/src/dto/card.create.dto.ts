2c27721917e52d61df8a0f8dd4809a19b2908bed
N-Barraza <nestor.barraza@b2crypto.com>
2024-12-11 17:54:40 -0500

    update card dto

--- libs/account/src/dto/card.create.dto.ts
+++ libs/account/src/dto/card.create.dto.ts
@@ -1,6 +1,6 @@
 
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsEnum, IsOptional, IsString, Min } from 'class-validator';
+import { IsBoolean, IsEnum, IsNumberString, IsOptional, IsString, Length } from 'class-validator';
 import CardTypesAccountEnum from '../enum/card.types.account.enum';
 import TypesAccountEnum from '../enum/types.account.enum';
 import { AccountCreateDto } from './account.create.dto';
@@ -40,7 +40,8 @@ export class CardCreateDto extends AccountCreateDto {
   name: string;
 
   @IsString()
-  @Min(0)
+  @IsNumberString()
+  @Length(4, 4)
   @IsOptional()
   @ApiProperty({
     required: false,
