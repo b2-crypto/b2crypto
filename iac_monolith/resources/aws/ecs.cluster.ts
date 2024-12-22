d64da40bdc4f202df6c86255f5613c0e2075cf8a
Alfredo Poveda <alfredo.poveda@b2crypto.com>
2024-12-22 14:43:53 -0500

    fix: 🐛 fix ci/cd

--- iac_monolith/resources/aws/ecs.cluster.ts
+++
@@ -1,6 +0,0 @@
-import * as aws from '@pulumi/aws';
-import { PROJECT_NAME, STACK } from '../../secrets';
-
-export const ecsCluster = aws.ecs.getClusterOutput({
-  clusterName: `${PROJECT_NAME}-${STACK}`,
-});
--- iac_shared/resources/aws/acm.certificate.ts
+++
@@ -1,6 +0,0 @@
-import * as aws from '@pulumi/aws';
-import { DOMAIN } from '../../secrets';
-
-export const acmCertificate = aws.acm.getCertificateOutput({
-  domain: DOMAIN,
-});
