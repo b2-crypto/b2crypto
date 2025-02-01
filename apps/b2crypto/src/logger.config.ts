cebe0d0d354e69212b12f84c5b9177957d104f77
Alfredo Poveda <alfredo.poveda@b2crypto.com>
2025-02-01 13:32:14 -0500

    wip: 🚧 work in progress

--- apps/b2crypto/src/logger.config.ts
+++ apps/b2crypto/src/logger.config.ts
@@ -29,8 +29,44 @@ export const loggerConfig = {
     }),
     autoLogging: true,
     serializers: {
-      req: () => undefined,
-      res: () => undefined,
+      req: (data) => {
+        const entriesBody = Object.entries(data?.body ?? {});
+        const entriesBodySanitized = entriesBody.map(([key, value]) => {
+          return /password|secret|key|token|apikey/i.test(key)
+            ? [key, '********']
+            : [key, value];
+        });
+
+        const bodySanitized = Object.fromEntries(entriesBodySanitized);
+
+        return {
+          ...data,
+          body: bodySanitized,
+          headers: {
+            ...data.headers,
+            [CORRELATION_ID_HEADER]: data.correlationId,
+          },
+        };
+      },
+      res: (data) => {
+        const entriesBody = Object.entries(data?.body ?? {});
+        const entriesBodySanitized = entriesBody.map(([key, value]) => {
+          return /password|secret|key|token|apikey/i.test(key)
+            ? [key, '********']
+            : [key, value];
+        });
+
+        const bodySanitized = Object.fromEntries(entriesBodySanitized);
+
+        return {
+          ...data,
+          body: bodySanitized,
+          headers: {
+            ...data.headers,
+            [CORRELATION_ID_HEADER]: data.correlationId,
+          },
+        };
+      },
     },
   },
 };
