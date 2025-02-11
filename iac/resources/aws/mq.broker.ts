9db3040abaffaf31cd384021f923ce232374376f
Alfredo Poveda <alfredo.poveda@b2crypto.com>
2025-02-11 13:23:01 -0500

    fix: 🐛 fixed fireblocks transactions

--- iac/resources/aws/mq.broker.ts
+++ iac/resources/aws/mq.broker.ts
@@ -32,7 +32,7 @@ export const mqBrokerRabbitMQ = new aws.mq.Broker(
       general: true,
       audit: false,
     },
-    autoMinorVersionUpgrade: false,
+    autoMinorVersionUpgrade: true,
     deploymentMode: MQ_DEPLOYMENT_MODE,
     authenticationStrategy: 'SIMPLE',
     tags: TAGS,
